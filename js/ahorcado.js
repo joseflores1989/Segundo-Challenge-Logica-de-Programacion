b4203ad32e5b933b233ce3d3f733c8d95460d70a
Jose <josemfa32@gmail.com>
2022-02-01 21:51:18 -0300

    Se corrigen errores

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -1,29 +1,24 @@
 function elegirPalabra(lista){
     var indicePalabra = Math.floor(Math.random()*lista.length);
+    console.log(indicePalabra);
     var palabraElegida = lista[indicePalabra];
+    console.log(palabraElegida);
     return palabraElegida;
 }
 
 function validarLetra(letraIngresada){
-    var listaLetrasValidas = "ABCDEFGHIJKLMNOPQRSTUVWXÑ";
-    var letraEsValida = false;
-    if (listaLetrasValidas.includes(letraIngresada)){
-        LetraEsValida = true;
-    }
-    return letraEsValida;
+    var listaLetrasValidas = "ABCDEFGHIJKLMNOPQRSTUVWXZÑ";
+    return listaLetrasValidas.includes(letraIngresada);
 }
 
 function buscarLetra(letra, palabra){
     listaIndices=[];
     for (var i = 0; i < palabra.length; i++){
-        if(palabra[i] = letra){
+        if(palabra[i] == letra){
             listaIndices.push(i);
         }
     }
     return listaIndices;
 }
-var listaPalabras = ["CARRO, SALCHICHA, AMIGO, LOMBRIZ"];
 
-var botonAgregarPalabra = document.querySelector("#nueva-palabra");
-var botonIniciarJuego = document.querySelector("#iniciar-juego");
 
